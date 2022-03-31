ad8b9b00eddcc9b09a15a2ecd28b19113a986bf1
mpplatisa <101156011+mpplatisa@users.noreply.github.com>
2022-03-30 18:28:05 -0700

    first commit

---
+++ node.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+    var greenButton = document.getElementById("green")
+    var orangeButton = document.getElementById("orange")
+    var redButton = document.getElementById("red")
+    var silverButton = document.getElementById("silver")
+    var blueButton = document.getElementById("blue");
+    
+}
+
+)
